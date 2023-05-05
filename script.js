1c4163e424b7f5f38a35f6f862f78638d34b6b5e
deshmukhlokesh123 <123466881+deshmukhlokesh123@users.noreply.github.com>
2023-05-06 01:47:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:[" ","Frontend Developer","Backend Developer","Web Developer","Full Stack Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
